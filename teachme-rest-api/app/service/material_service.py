d676546d814ca113cfed0f838387dfc0de0c4648
kiditz <kiditzbastara@gmail.com>
2018-06-03 21:08:06 +0700

    Improve document api for add material

--- teachme-rest-api/app/service/material_service.py
+++ teachme-rest-api/app/service/material_service.py
@@ -1,7 +1,8 @@
 from slerp.logger import logging
-from slerp.validator import Number, Blank, Key
+from slerp.validator import Number, Blank, Key, ValidationException
+from slerp.string_utils import is_blank, is_digit
 
-from entity.models import Material
+from entity.models import Material, MaterialTopic
 
 log = logging.getLogger(__name__)
 
@@ -11,9 +12,24 @@ class MaterialService(object):
 		super(MaterialService, self).__init__()
 	
 	@Blank(['title', 'description', 'type'])
-	@Number(['document_id', 'user_id', 'price', "topic_id"])
+	@Number(['document_id', 'user_id', 'price'])
 	def add_material(self, domain):
+		if 'topic_id' not in domain:
+			topic = MaterialTopic({'user_id': domain['user_id']})
+			if is_blank(domain['name']):
+				raise ValidationException('required.value.name')
+			if 'level_id' in domain and is_digit(domain['level_id']):
+				topic.level_id = domain['level_id']
+			if 'class_id' in domain and is_digit(domain['class_id']):
+				topic.class_id = domain['class_id']
+			topic.name = domain['name']
+			topic.save()
+		else:
+			topic = MaterialTopic.query.get(domain['topic_id'])
+			domain.pop('topic_id')
+		pass
 		material = Material(domain)
+		material.topic_id = topic.id
 		material.save()
 		return {'payload': material.to_dict()}
 	
@@ -33,7 +49,7 @@ class MaterialService(object):
 	def get_material_by_topic_id(self, domain):
 		page = int(domain['page'])
 		size = int(domain['size'])
-		material_q = Material.query.filter_by(topic_id=domain['topic_id']).order_by(Material.id.desc())\
+		material_q = Material.query.filter_by(topic_id=domain['topic_id']).order_by(Material.id.desc()) \
 			.paginate(page, size, error_out=False)
 		material_list = list(map(lambda x: x.to_dict(), material_q.items))
 		return {'payload': material_list, 'total': material_q.total, 'total_pages': material_q.pages}
